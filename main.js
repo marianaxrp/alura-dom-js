172c326b376703fcce0f673cc40d881ba620b725
Mariana Xavier <c0606581@vale.com>
2021-02-07 18:41:23 -0300

    added function to store data in local storage and refactored code

--- main.js
+++ main.js
@@ -1,28 +1,5 @@
-// (() => {
-import BotaoConcluir from './components/ConcluirTarefa.js';
-import BotaoDeletar from './components/DeletarTarefa.js';
-
-    const criarTarefa = (event) => {
-    event.preventDefault();
-
-    const lista = document.querySelector('[data-list]');
-    const input = document.querySelector('[data-form-input]');
-    const valor = input.value;
-    
-    const tarefa = document.createElement('li');
-    tarefa.classList.add('task');
-    const conteudo = `<p class="content">${valor}</p>`;
-    
-    tarefa.innerHTML = conteudo;
-    
-    tarefa.appendChild(BotaoConcluir());
-    tarefa.appendChild(BotaoDeletar());
-    lista.appendChild(tarefa);
-    input.value = "";
-}
+import {handleNovoItem} from './components/CriaTarefa.js'
 
 const novaTarefa = document.querySelector('[data-form-button]');
 
-novaTarefa.addEventListener('click', criarTarefa);
-
-// })();
+novaTarefa.addEventListener('click', handleNovoItem);
